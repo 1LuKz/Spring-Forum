f83dc523e93f4644a56ecae68a8e4e90f8405b77
1LuKz <lucasedu292003@gmail.com>
2025-11-17 11:33:17 -0300

    First commit

---
+++ Spring-Forum/src/main/java/com/springforum/Spring/Forum/SpringForumApplication.java
@@ -0,0 +1,13 @@
+package com.springforum.Spring.Forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForumApplication.class, args);
+	}
+
+}
