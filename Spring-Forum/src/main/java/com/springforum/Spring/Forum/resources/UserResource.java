148ae49c7afa184fee911efb14ddd57af73d8793
1LuKz <lucasedu292003@gmail.com>
2025-11-18 09:25:59 -0300

    Insert user with POST

--- Spring-Forum/src/main/java/com/springforum/Spring/Forum/resources/UserResource.java
+++ Spring-Forum/src/main/java/com/springforum/Spring/Forum/resources/UserResource.java
@@ -6,11 +6,10 @@ import com.springforum.Spring.Forum.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,12 +24,20 @@ public class UserResource {
     public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = userService.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-        return ResponseEntity.status(HttpStatus.OK).body(listDto);
+        return ResponseEntity.ok().body(listDto);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
         User user = userService.findById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(new UserDTO(user));
+        return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO){
+        User user = userService.fromDTO(userDTO);
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- Spring-Forum/src/main/java/com/springforum/Spring/Forum/services/UserService.java
+++ Spring-Forum/src/main/java/com/springforum/Spring/Forum/services/UserService.java
@@ -1,6 +1,7 @@
 package com.springforum.Spring.Forum.services;
 
 import com.springforum.Spring.Forum.domain.User;
+import com.springforum.Spring.Forum.dto.UserDTO;
 import com.springforum.Spring.Forum.repository.UserRepository;
 import com.springforum.Spring.Forum.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(String id){
         return userRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User user){
+        return userRepository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO){
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
