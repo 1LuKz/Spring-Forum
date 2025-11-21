85607c5197291bb56ef3876075d6be5918a87a8f
1LuKz <lucasedu292003@gmail.com>
2025-11-21 08:15:42 -0300

    Author data with DTO

--- Spring-Forum/src/main/java/com/springforum/Spring/Forum/config/Instantiation.java
+++ Spring-Forum/src/main/java/com/springforum/Spring/Forum/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.springforum.Spring.Forum.config;
 
 import com.springforum.Spring.Forum.domain.Post;
 import com.springforum.Spring.Forum.domain.User;
+import com.springforum.Spring.Forum.dto.AuthorDTO;
 import com.springforum.Spring.Forum.repository.PostRepository;
 import com.springforum.Spring.Forum.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User zutomayo = new User(null, "Zutto mayonaka de iinoni", "zutto@gmail.com");
         User miku = new User(null, "Hatsune Miku", "miku@gmail.com");
 
-        Post firstPost = new Post(null, simpleDateFormat.parse("29/01/2025"), "New albums", "I'm listening to zutomayo's new album!", lucas);
-        Post secondPost = new Post(null, simpleDateFormat.parse("05/10/2024"), "New music", "Guys the new single is coming soon", miku);
-
         userRepository.saveAll(Arrays.asList(lucas, zutomayo, miku));
+
+        Post firstPost = new Post(null, simpleDateFormat.parse("29/01/2025"), "New albums", "I'm listening to zutomayo's new album!", new AuthorDTO(lucas));
+        Post secondPost = new Post(null, simpleDateFormat.parse("05/10/2024"), "New music", "Guys the new single is coming soon", new AuthorDTO(miku));
+
         postRepository.saveAll(Arrays.asList(firstPost, secondPost));
     }
 }
