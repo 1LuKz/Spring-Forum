2cb738fcbe59f720dd4eda76d1388f8afa2daf8c
1LuKz <lucasedu292003@gmail.com>
2025-11-21 15:41:36 -0300

    Consultation with multiple criteria

--- Spring-Forum/src/main/java/com/springforum/Spring/Forum/resources/PostResource.java
+++ Spring-Forum/src/main/java/com/springforum/Spring/Forum/resources/PostResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -32,4 +33,17 @@ public class PostResource {
         List<Post> list = postService.findByTitle(text);
         return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value ="text", defaultValue = "") String text,
+            @RequestParam(value ="minDate", defaultValue = "") String minDate,
+            @RequestParam(value ="maxDate", defaultValue = "") String maxDate){
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
